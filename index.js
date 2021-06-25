5250dcca474aba8c1199f1e28414ee6adaaa98d7
Kristian Freeman <kristian@kristianfreeman.com>
2021-06-25 12:33:07 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
-
 async function handleRequest(request) {
   const response = await fetch(request)
-  const headers = new Headers(response.headers)
-
-  // Add a header
-  headers.append("x-workers-hello", "Hello from Cloudflare Workers")
-
+  
+  // Clone the response so that it's no longer immutable
+  const newResponse = new Response(response.body, response)
+  
+  // Add a custom header with a value
+  newResponse.headers.append("x-workers-hello", "Hello from Cloudflare Workers")
+  
   // Delete headers
-  headers.delete("x-header-to-delete")
-  headers.delete("x-header2-to-delete")
-
-  // Change a header
-  headers.set("x-header-to-change", "NewValue")
-
-  const newResponse = new Response(response.body, {
-    ...response,
-    headers
-  })
-
+  newResponse.headers.delete("x-header-to-delete")
+  newResponse.headers.delete("x-header2-to-delete")
+  
+  // Adjust the value for an existing header
+  newResponse.headers.set("x-header-to-change", "NewValue")
+  
   return newResponse
 }
